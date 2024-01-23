09befbe2b369719157e413cc405287d2d7a1cff3
Natan Xavier <natanxaviersiquera@gmail.com>
2024-01-23 20:01:45 -0300

    Create index.php

---
+++ view/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centro Cultural Capoeira Ventre Livre</title>
+    <?php include "assets/css/styles.php" ?>
+    <link rel="shortcut icon" href="assets/img/favicon.jpg" type="image/x-icon">
+</head>
+
+<body>
+    <header>
+        <nav class="nav-bar">
+            <img width="77" height="70" src="assets/img/favicon.jpg" alt="logo">
+            <ul class="nav-itens">
+                <li><a href="#historia">Historia</a></li>
+                <li><a href="#">Mestres</a></li>
+                <li><a href="#contatos">Contatos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1 class="titulo">Centro Cultural Capoeira Ventre Livre</h1>
+        <section id="historia">
+            <div class="img">
+                <img class="logo-grande" src="assets/img/Logo_cccventrelivre.jpg" alt="Logo Ventre Livre">
+            </div>
+        </section>
+    </main>
+    <footer class="rodapé">
+        <ul class="redes-sociais">
+            <li>Facebook</li>
+            <li>Instagram</li>
+        </ul>
+        <ul>
+            <li>email</li>
+            <li>Assuntos gerais - cccventrelivre@gmail.com</li>
+            <li>Presidente do grupo - cccventrepresidente@gmail.com</li>
+            <li>Gerenrecia de eventos - cccventreevento@gmail.com</li>
+        </ul>
+    </footer>
+</body>
+
+</html>
+
+<?php ?>
